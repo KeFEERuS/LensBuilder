a4b2c9106d76a02ef858ee9fa0d396dcb9dbf20c
Red Tritium <feer43@gmail.com>
2018-10-24 20:46:14 +0300

    Правки GUI, новый клас с расчётными данными.

---
+++ src/ProcessingData.java
@@ -0,0 +1,22 @@
+public class ProcessingData {
+
+    private double d;
+
+    public double getLimitDiameter(double x) {
+        if (x <= 2) {
+            return d = (x + 0.4);
+        } else if (x <= 4) {
+            return d = (x + 0.6);
+        } else if (x <= 10) {
+            return d = (x + 0.8);
+        } else if (x <= 15) {
+            return d = (x + 1);
+        } else if (x <= 25) {
+            return d = (x + 1.2);
+        } else if (x <= 65) {
+            return d = (x + 1.5);
+        } else if (x <= 100) {
+            return d = (x + 2.5);
+        } else return d = (x + 4);
+    }
+}
