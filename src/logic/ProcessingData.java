2dbe0ae90816d186d9944d2f3e220bd31175656a
Red Tritium <feer43@gmail.com>
2019-03-04 10:51:07 +0300

    Опять добавил кучу всего.

--- src/logic/ProcessingData.java
+++ src/logic/ProcessingData.java
@@ -2,8 +2,16 @@ package logic;
 
 public class ProcessingData {
 
-    private double d;
+    private double d; //диаметр готовой линзы
+    private double mass; //масса итоговой заготовки
+    private double price; //цена заготовки
 
+    /**
+     * Этот метод возвращает оптимальный диаметр для заготовки
+     * в зависимости от деаметра готовой линзы.
+     * На вход подаётся диаметр готовой линзы,
+     * На выходе получаем оптимальный диаметр заготовки.
+     */
     public double getLimitDiameter(double x) {
         if (x <= 2) {
             return d = (x + 0.4);
@@ -20,5 +28,33 @@ public class ProcessingData {
         } else if (x <= 100) {
             return d = (x + 2.5);
         } else return d = (x + 4);
+    }
+
+    /**
+     * Этот метод возвращает плотность материала.
+     * На вход подаётся строка с названием марки стекла,
+     * метод находит данную мару стекла в базе данных и возвращает
+     * его плотность в г/см^3
+     */
+    private double getDensity(String lensMaterial) {
+
+    }
+
+    /**
+     * Этот метод возвращает массу заготовки.
+     * На вход подаются объём заготовки и марка стекла
+     */
+    public double getMassLens(double volume, String sort) {
+        mass = getDensity(sort) * volume;
+        return mass;
+    }
+
+    /**
+     * Метод для возврата стоимости заготовки.
+     * На вход подаются масса заготовки и цена за Кг материала
+     */
+    public double getPriceLens(double mass, double price) {
+        this.price = mass * price;
+        return this.price;
     }
 }
